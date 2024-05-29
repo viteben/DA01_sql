5fa494aa3c0f0db894044ec25a3028e78a49d946
viteben <leviethoang200511@gmail.com>
2024-05-29 21:46:25 +0700

    Update Day3_baitap.sql

--- Day3_baitap.sql
+++ Day3_baitap.sql
@@ -34,3 +34,16 @@ WHERE NOT referee_id = 2 OR referee_id is null
 --EX11
 SELECT name, population, area FROM World
 WHERE area >= 3000000 OR population >= 25000000
+--EX12
+SELECT DISTINCT author_id AS id FROM Views 
+WHERE author_id = viewer_id
+ORDER BY author_id
+--EX13
+SELECT part, assembly_step FROM parts_assembly
+WHERE finish_date IS NULL;
+--EX14
+SELECT * FROM lyft_drivers
+WHERE yearly_salary <= 30000 OR yearly_salary >= 70000
+--EX15
+SELECT advertising_channel FROM uber_advertising
+WHERE money_spent > 100000 AND year = 2019
