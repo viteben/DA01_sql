c4af1f3440d78741b04df800aba8d19319785fe6
viteben <leviethoang200511@gmail.com>
2024-06-12 23:00:20 +0700

    Update Mid-course test.sql

--- Mid-course test.sql
+++ Mid-course test.sql
@@ -1,1 +1,7 @@
 
+select  country||','|| city, SUM(amount) from country a join city b on a.country_id=b.country_id
+                        join address c on b.city_id=c.city_id
+                        join customer d on c.address_id=d.address_id
+                        join payment e on d.customer_id=e.customer_id
+group by e.customer_id,d.address_id,b.city_id,a.country_id
+order by sum(amount) 
