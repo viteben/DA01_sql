3967c435a8227ceb7e53069a80803996902f723a
viteben <leviethoang200511@gmail.com>
2024-06-12 20:42:58 +0700

    Update Day11_baitap.sql

--- Day11_baitap.sql
+++ Day11_baitap.sql
@@ -44,9 +44,4 @@ ORDER BY a.page_id
 
 
 
-select  country||','|| city, SUM(amount) from country a join city b on a.country_id=b.country_id
-                        join address c on b.city_id=c.city_id
-                        join customer d on c.address_id=d.address_id
-                        join payment e on d.customer_id=e.customer_id
-group by e.customer_id,d.address_id,b.city_id,a.country_id
-order by sum(amount) 
+
